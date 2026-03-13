cd2031fa68d1573a3722e5921747852d9e848200
MarlyB5 <marlybah051205@gmail.com>
2026-03-13 01:55:41 +0000

    simple song class

--- src/main/java/org/Song.java
+++ src/main/java/org/Song.java
@@ -1,4 +1,31 @@
 package org;
-
+//songs class for storing songs
 public class Song {
+
+    private String title;
+    private String artist;
+    private String fileLocation;
+
+    public Song(String title, String artist, String fileLocation) {
+        this.title = title;
+        this.artist = artist;
+        this.fileLocation = fileLocation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getFileLocation() {
+        return fileLocation;
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist;
+    }
 }
