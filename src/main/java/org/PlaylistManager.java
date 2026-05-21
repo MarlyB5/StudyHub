9d4a8b382ef5be1b9cf0ad1f2b0469111effe942
Marly <marlybah051205@gmail.com>
2026-05-21 08:04:18 +0100

    Created Playlist Manager class

---
+++ src/main/java/org/PlaylistManager.java
@@ -0,0 +1,32 @@
+package org;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+public class PlaylistManager {
+
+    private final ObservableList<Song> songs;
+
+    public PlaylistManager() {
+        songs = FXCollections.observableArrayList();
+
+    }
+
+    private void loadDefaultSongs(){
+        songs.add(new Song("Sample Song 1", "Unknown Artist", "/SampleSong1.mp3"));
+        songs.add(new Song("Sample Song 2", "Unknown Artist", "/SampleSong2.mp3"));
+
+    }
+
+    public ObservableList<Song> getSongs(){
+        return songs;
+    }
+
+    public void removeSong(Song song){
+        songs.remove(song);
+    }
+
+    public void addSong(Song song){
+        songs.add(song);
+    }
+}
